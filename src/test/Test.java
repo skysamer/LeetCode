5860e92a85b08a0c71481a7cf1673466438e2356
skysamer <skyslayer123@naver.com>
2021-04-06 18:22:11 +0900

    test

---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.Collections;
+import java.util.HashMap;
+
+public class Test {
+
+	public static void main(String[] args) {
+		HashMap<String, String> map=new HashMap<String, String>();
+		Collections collection;
+		System.out.println("Hello world");
+
+	}
+
+}
