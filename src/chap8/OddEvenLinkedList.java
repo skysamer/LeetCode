3a16905e1e73fb5f65b7333ca87b374109a22179
skysamer <skyslayer123@naver.com>
2021-06-03 21:28:47 +0900

    oddEvenLinkedList

---
+++ src/chap8/OddEvenLinkedList.java
@@ -0,0 +1,28 @@
+package chap8;
+
+public class OddEvenLinkedList {
+	
+	public ListNode oddEvenList(ListNode head) {
+		if(head==null) {
+			return null;
+		}
+		
+		ListNode odd=head;
+		ListNode even=head.next;
+		ListNode evenHead=even;
+		
+		while(even!=null && even.next!=null) {
+			odd.next=odd.next.next;
+			even.next=even.next.next;
+			odd=odd.next;
+			even=even.next;
+		}
+		odd.next=evenHead;
+        return head;
+    }
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
